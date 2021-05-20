8d69f6cc1374361c6f6e5f217db9f98edc617013
Lore <loredotexe@pocketplugin.dev>
2021-05-19 20:29:33 -0400

    Create FactionsEvents.php

---
+++ src/LoreDOTexe/FactionsUI/FactionsEvents.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace LoreDOTexe\FactionsUI;
+
+use pocketmine\Player;
+use pocketmine\event\Listener;
+use LoreDOTexe\FactionsUI\FactionsManager;
+
+class FactionsEvents implements Listener{
+    
+}
