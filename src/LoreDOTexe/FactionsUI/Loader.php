ddc0bd1447f013ffbb7dab92059178be3d2904ee
Lore <loredotexe@pocketplugin.dev>
2021-05-19 20:24:43 -0400

    Update Loader.php

--- src/LoreDOTexe/FactionsUI/Loader.php
+++ src/LoreDOTexe/FactionsUI/Loader.php
@@ -2,13 +2,26 @@
 
 namespace LoreDOTexe\FactionsUI;
 
-use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
 use pocketmine\utils\Config;
+use pocketmine\command\Command;
+use pocketmine\plugin\PluginBase;
+
+use LoreDOTexe\FactionsUI\FactionsCommand;
+use LoreDOTexe\FactionsUI\FactionsManager;
+use LoreDOTexe\FactionsUI\FactionsEvents;
+
+private $config;
 
 class Loader extends PluginBase{
-  
+
     public function onEnable(){
 
-    }
+        $this->saveDefaultConfig();
+        $config = $this->getConfig();
+      
+        $this->getServer()->getPluginManager()->registerEvents(new FactionsEvents($this), $this);
+        $this->getServer()->getCommandMap()->register($config->get("command-name"), new FactionsCommand($this));
 
+    }
 }
